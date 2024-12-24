ea5d949e2775b841c3aa15c4b85b7aa98541f89c
buyaramadhan <ytchochote@gmail.com>
2024-12-24 11:18:02 +0700

    Memperbarui DokterDao

--- app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -4,18 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface DokterDao{
-    @Insert
-    suspend fun insertDokter(dokter: Dokter): Long;
+interface DokterDao {
 
-    @Query("SELECT * FROM dokter")
-    suspend fun getAllDokters(): List<Dokter>;
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
 
     @Query("SELECT * FROM dokter WHERE id = :id")
-    suspend fun getDokterById(id: Int): Dokter?;
+    fun getDokterById(id: Int): Flow<Dokter?>
 
 }
-
-
