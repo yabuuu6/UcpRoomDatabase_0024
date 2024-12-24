7b93e8a7ef92bf89806197e330af545a453c9df6
buyaramadhan <ytchochote@gmail.com>
2024-12-24 11:29:36 +0700

    Menambahkan objek Spesialis untuk menyimpan daftar opsi spesialisasi dokter

---
+++ app/src/main/java/com/example/ucp2/data/entity/Spesialis.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.data.entity
+
+object Spesialis {
+    val option = listOf(
+        "Dokter Umum",
+        "Dokter Anak",
+        "Dokter Gigi",
+        "Dokter Jantung",
+        "Dokter THT"
+    )
+}
