81fab0d6a85c89cd0ced90c4535c49b96aa8afa9
buyaramadhan <ytchochote@gmail.com>
2024-12-24 11:18:42 +0700

    Memperbarui entitas dokter

--- app/src/main/java/com/example/ucp2/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Dokter.kt
@@ -4,11 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "dokter")
-data class Dokter(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val Nama: String,
-    val Spesialis: String,
-    val Klinik: String,
-    val NoTelpn: String,
-    val JamKerja: String
+data class Dokter (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val telepon: String,
+    val jamKerja: String
 )
